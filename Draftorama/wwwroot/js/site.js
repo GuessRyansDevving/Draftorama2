b6405d770301791b2d465a69f127eb28f8884e6e
Ryan Swedenburg <ryandevsafe@gmail.com>
2017-04-17 17:41:36 -0400

    Progress made on intro

--- Draftorama/wwwroot/js/site.js
+++ Draftorama/wwwroot/js/site.js
@@ -1,5 +1,5 @@
-﻿// Write your Javascript code.
-function draftBotText() {
-    for (i = 0; i < ) {
-    }
-}
+﻿$("#text-draftbot-intro-1").delay(500).animate({ opacity: 1 }, 700);
+$("#text-draftbot-intro-2").delay(2500).animate({ opacity: 1 }, 700);
+$("#buttons-draftbot-intro-1").delay(3000).animate({ opacity: 1 }, 700);
+$("#buttons-draftbot-intro-2").delay(3500).animate({ opacity: 1 }, 700);
+$("#buttons-draftbot-intro-3").delay(4000).animate({ opacity: 1 }, 700);
