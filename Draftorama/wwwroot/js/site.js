636768e5c64a4591d6a8f8be912a265feacfc749
Ryan Swedenburg <ryandevsafe@gmail.com>
2017-04-30 17:31:42 -0400

    blah

--- Draftorama/wwwroot/js/site.js
+++ Draftorama/wwwroot/js/site.js
@@ -1,5 +1,10 @@
-﻿$("#text-draftbot-intro-1").delay(500).animate({ opacity: 1 }, 700);
-$("#text-draftbot-intro-2").delay(2500).animate({ opacity: 1 }, 700);
-$("#buttons-draftbot-intro-1").delay(3000).animate({ opacity: 1 }, 700);
-$("#buttons-draftbot-intro-2").delay(3500).animate({ opacity: 1 }, 700);
-$("#buttons-draftbot-intro-3").delay(4000).animate({ opacity: 1 }, 700);
+﻿$("#text-draftbot-intro-1").delay(500).animate({ opacity: 1 }, 400);
+$("#text-draftbot-intro-2").delay(2500).animate({ opacity: 1 }, 400);
+$("#buttons-draftbot-intro-1").delay(3000).animate({ opacity: 1 }, 400);
+$("#buttons-draftbot-intro-2").delay(3500).animate({ opacity: 1 }, 400);
+$("#buttons-draftbot-intro-3").delay(4000).animate({ opacity: 1 }, 400);
+
+$('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
